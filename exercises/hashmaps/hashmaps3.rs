bf9b91fc771abb703778ff5c93b7fe0ec8bbec5c
NewAlice <819428005@qq.com>
2025-02-06 01:33:25 +0800

    fix:bug

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -17,6 +17,7 @@
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
+#[derive(Default)]
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
@@ -37,11 +38,11 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
-        let TeamScores{ goals_scored,goals_conceded} = scores.entry(team_1_name).or_default();
+        let Team{ goals_scored,goals_conceded} = scores.entry(team_1_name).or_default();
         *goals_scored += team_1_score;
         *goals_conceded += team_2_score;
         
-        let TeamScores{ goals_scored,goals_conceded} = scores.entry(team_2_name).or_default();
+        let Team{ goals_scored,goals_conceded} = scores.entry(team_2_name).or_default();
         *goals_scored += team_2_score;
         *goals_conceded += team_1_score;
     }
